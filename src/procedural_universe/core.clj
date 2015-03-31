d1f15c0ef64ff0caebb0cf7b05a286de6e44ff5b
John Cowie <john.a.cowie@gmail.com>
2015-03-31 20:45:37 +0100

    First pass of a procedurally generated universe

--- src/procedural_universe/core.clj
+++ src/procedural_universe/core.clj
@@ -1,6 +1,24 @@
 (ns procedural-universe.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def order [:universe :galaxy :star :solar-system :planet :continent :country :city])
+
+(defn next-layer [k]
+  (second (drop-while #(not= k %) (cycle order))))
+
+(defn update-leaves
+  ([f m layer]
+     (if (empty? m)
+       (f layer m)
+       (into {} (map (fn [[k v]] [k (update-leaves f v (next-layer layer) )]) m))))
+  ([f m]
+     (update-leaves f m :universe)))
+
+(defn append-to-k [k i]
+  (keyword (str (name k) "-" i)))
+
+(defn add-stuff [layer m]
+  (assoc m (append-to-k layer (rand-int 10000)) {} (append-to-k layer (rand-int 10000)) {}))
+
+(def update (partial update-leaves add-stuff))
+
+(nth (iterate update {}) (count order))
