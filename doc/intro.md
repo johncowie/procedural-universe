224586c287aeda81782b19b6ab411e396a7cd2ad
John Cowie <john.a.cowie@gmail.com>
2015-03-31 19:43:17 +0100

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to procedural-universe
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/procedural_universe/core_test.clj
@@ -0,0 +1,7 @@
+(ns procedural-universe.core-test
+  (:use clojure.test
+        procedural-universe.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
